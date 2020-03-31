f8579d82af2d92616fb2a9a722e6ba76fddac1d3
guochao <>
2020-03-31 18:26:25 +0800

    first commit

---
+++ ChooseLocation/ChooseLocation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ChooseLocation
+//
+//  Created by 郭超 on 2020/3/31.
+//  Copyright © 2020 gucohao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ChooseLocation/ChooseLocation/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ChooseLocation
+//
+//  Created by 郭超 on 2020/3/31.
+//  Copyright © 2020 gucohao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
